5c09619b827f54c344ef98f3cc5702dc4ab48ab2
Vishwa Patel <vishwa@Vishwas-MacBook-Air.local>
2012-05-15 06:52:09 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VishwaApp::Application.config.secret_token = '1e33af52d5b8d2d5eb5c64d7e4bf0cb0c86ac5e6d18aed412ef69f32d919fc5257af960159aa1aef18854ec1da2826c440c71f01225c2c215c10e74b9a6593dd'
